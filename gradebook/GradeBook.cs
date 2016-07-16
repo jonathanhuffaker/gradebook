1677c7cb9817fc9781cce3b8a8421dedcb205df9
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-07-15 22:02:45 -0500

    book.addgrade implemented

--- gradebook/GradeBook.cs
+++ gradebook/GradeBook.cs
@@ -13,6 +13,6 @@ namespace Grades
             grades.Add(grade);
         }
 
-        List<float> grades;
+        List<float> grades = new List<float>();
     }
 }
--- gradebook/Program.cs
+++ gradebook/Program.cs
@@ -11,6 +11,9 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
         }
     }
 }
